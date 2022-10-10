004541b231520838053f6b6dce24f1e84b2a1af4
v-crn <var.cyrano@gmail.com>
2022-10-10 18:59:41 +0900

    add test_finder.py

--- tests/vnlpkit/utils/test_finder.py
+++ tests/vnlpkit/utils/test_finder.py
@@ -1,6 +1,7 @@
+import time
 
 from vnlpkit.utils.finder import find_path
-import time
+
 
 def test_find_path__cache_is_faster() -> None:
     partial_path = "test_finder.py"
@@ -12,12 +13,16 @@ def test_find_path__cache_is_faster() -> None:
     start_time = time.time()
     find_path(partial_path=partial_path, use_cache=True)
     with_cache_time_1 = time.time() - start_time
-    diff_without_cache_time_and_with_cache_time_1 = without_cache_time - with_cache_time_1
+    diff_without_cache_time_and_with_cache_time_1 = (
+        without_cache_time - with_cache_time_1
+    )
 
     start_time = time.time()
     find_path(partial_path=partial_path, use_cache=True)
     with_cache_time_2 = time.time() - start_time
-    diff_without_cache_time_and_with_cache_time_2 = without_cache_time - with_cache_time_2
+    diff_without_cache_time_and_with_cache_time_2 = (
+        without_cache_time - with_cache_time_2
+    )
 
     print()
     print(f"Found filepath: {filepath}")
@@ -26,7 +31,11 @@ def test_find_path__cache_is_faster() -> None:
     print(f"without_cache_time: {without_cache_time}")
     print(f"with_cache_time_1: {with_cache_time_1}")
     print(f"with_cache_time_2: {with_cache_time_2}")
-    print(f"diff_without_cache_time_and_with_cache_time_1: {diff_without_cache_time_and_with_cache_time_1}")
-    print(f"diff_without_cache_time_and_with_cache_time_2: {diff_without_cache_time_and_with_cache_time_2}")
+    print(
+        f"diff_without_cache_time_and_with_cache_time_1: {diff_without_cache_time_and_with_cache_time_1}"
+    )
+    print(
+        f"diff_without_cache_time_and_with_cache_time_2: {diff_without_cache_time_and_with_cache_time_2}"
+    )
 
     assert with_cache_time_1 > with_cache_time_2
